bcf165baaadf18152376a76c265f59f5f226d36d
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2014-06-13 16:23:13 +0300

    Sunglasses Homework

---
+++ C# Basics/PrimitiveDataTypesAndVar/17.Sunglasses/Sunglasses.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _17.Sunglasses
+{
+    class Sunglasses
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write(new string('*', n * 2));
+            Console.Write(new string(' ', n));
+            Console.Write(new string('*', n * 2));
+            Console.WriteLine();
+            
+            for (int i = 0; i < n - 2; i++)
+            {
+                
+                Console.Write(new string('*', 1));
+                Console.Write(new string('/', n*2-2));
+                Console.Write(new string('*', 1));
+
+                if (i == (n-2)/2)
+                {
+                    Console.Write(new string('|', n));
+                }
+                else
+                {
+                    Console.Write(new string(' ', n)); 
+                }
+
+                Console.Write(new string('*', 1));
+                Console.Write(new string('/', n * 2 - 2));
+                Console.Write(new string('*', 1));
+                Console.WriteLine();
+                
+            }
+
+            Console.Write(new string('*', n * 2));
+            Console.Write(new string(' ', n));
+            Console.Write(new string('*', n * 2));
+            Console.WriteLine();
+        }
+    }
+}
