204bdecd40e6abba935c7512893ea31e32ba8f13
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2014-06-13 14:21:59 +0300

    Intro Programming Homework

---
+++ C# Basics/IntroProgramming/07.PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PrintFirstAndLastName
+{
+    class PrintFirstAndLastName
+    {
+        static void Main()
+        {
+            //Problem 7.	Print First and Last Name
+            //Create console application that prints your first and last name, each at a separate line.
+
+            Console.WriteLine("Peshko");
+            Console.WriteLine("Goshkov");
+        }
+    }
+}
---
+++ C# Basics/IntroProgramming/14.CurrentDateAndTime/CurrentDateAndTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _14.CurrentDateAndTime
+{
+    class CurrentDateAndTime
+    {
+        static void Main()
+        {
+            //Current Date and Time
+            //Create a console application that prints the current date and time. Find in Internet how.
+
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine(now);
+        }
+    }
+}
