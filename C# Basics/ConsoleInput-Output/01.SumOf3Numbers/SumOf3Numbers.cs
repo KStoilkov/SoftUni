eb5d4613f309097f123fd48aa9da9faae5648163
KStoilkov <K.I.Stoilkov@gmail.com>
2014-07-06 17:50:22 +0300

    Corrected

--- C# Basics/ConsoleInput-Output/01.SumOf3Numbers/SumOf3Numbers.cs
+++ C# Basics/ConsoleInput-Output/01.SumOf3Numbers/SumOf3Numbers.cs
@@ -6,7 +6,7 @@ namespace _01.SumOf3Numbers
     {
         static void Main()
         {
-            // Write a program that reads 3 real numbers from the console and prdoubles their sum.
+            // Write a program that reads 3 real numbers from the console and prints their sum.
 
             Console.Write("a: ");
             double a = double.Parse(Console.ReadLine());
