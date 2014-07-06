d8a0ff4161a9c67039c25dd372658cfeb53d885f
KStoilkov <K.I.Stoilkov@gmail.com>
2014-07-06 17:03:27 +0300

    Conditional Statements Homework

---
+++ C# Basics/ConditionalStatements/04.MultiplicationSign/MultiplicationSign.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.MultiplicationSign
+{
+    class MultiplicationSign
+    {
+        static void Main()
+        {
+            // Write a program that shows the sign (+, - or 0) of the product of three real numbers, without calculating it. 
+            // Use a sequence of if operators. 
+            Console.Write("a: ");
+            double a= double.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (a < 0)
+            {
+                if (b < 0)
+                {
+                    if (c < 0)
+                    {
+                        Console.WriteLine("result: -");
+                    }
+                    else
+                    {
+                        Console.WriteLine("result: +");
+                    }
+                }
+                else
+                {
+                    if (c < 0)
+                    {
+                        Console.WriteLine("result: -");
+                    }
+                    else
+                    {
+                        Console.WriteLine("result: +");
+                    }
+                }
+            }
+            else if (a > 0)
+            {
+                if (b < 0)
+                {
+                    if (c < 0)
+                    {
+                        Console.WriteLine("result: +");
+                    }
+                    else
+                    {
+                        Console.WriteLine("result: -");
+                    }
+                }
+                else
+                {
+                    if (c < 0)
+                    {
+                        Console.WriteLine("result: -");
+                    }
+                    else
+                    {
+                        Console.WriteLine("result: +");
+                    }
+                }
+            }
+            else if ((a == 0) || (b == 0) || (c == 0))
+            {
+                Console.WriteLine("result: 0");
+            }
+
+
+        }
+    }
+}
---
+++ C# Basics/ConditionalStatements/05.TheBiggestOf3Numbers/TheBiggestOf3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.TheBiggestOf3Numbers
+{
+    class TheBiggestOf3
+    {
+        static void Main()
+        {
+            // Write a program that finds the biggest of three numbers. 
+
+            Console.Write("a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("biggest: " + a);
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("biggest: " + b);
+            }
+            else if (c > a && c > b)
+            {
+                Console.WriteLine("biggest: " + c);
+            }
+            else
+            {
+                Console.WriteLine("biggest: 0" );
+            }
+        }
+    }
+}
