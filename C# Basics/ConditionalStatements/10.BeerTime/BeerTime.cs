bfe54ee100863bcc44461916b95dad547ed9f576
KStoilkov <K.I.Stoilkov@gmail.com>
2014-07-07 15:06:45 +0300

    Homework*

---
+++ C# Basics/ConditionalStatements/10.BeerTime/BeerTime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace _10.BeerTime
+{
+    class BeerTime
+    {
+        static void Main()
+        {
+            // A beer time is after 1:00 PM and before 3:00 AM. Write a program that enters a time in format “hh:mm tt” 
+            // (an hour in range [01...12], a minute in range [00…59] and AM / PM designator) and prints “beer time” or 
+            // “non-beer time” according to the definition above or “invalid time” if the time cannot be parsed. 
+            // Note that you may need to learn how to parse dates and times. 
+
+            Console.Write("Enter time (h:mm tt): ");
+            string inputedTime = Console.ReadLine();
+            DateTime date = new DateTime();
+
+            bool check = DateTime.TryParseExact(inputedTime, "h:mm tt", CultureInfo.InvariantCulture,DateTimeStyles.None, out date);
+
+            DateTime after = DateTime.Parse("1:00 PM");
+            DateTime before = DateTime.Parse("3:00 AM");
+            if (check == true)
+            {
+                if (date >= after || date <= before)
+                {
+                    Console.WriteLine("beer time");
+                }
+                else
+                {
+                    Console.WriteLine("non-beer time");
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid time");
+            }
+        }
+    }
+}
---
+++ C# Basics/ConditionalStatements/11.NumberAsWords/NumberAsWords.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace _11.NumberAsWords
+{
+    class NumberAsWords
+    {
+        
+        static void Main()
+        {
+            // Write a program that converts a number in the range [0…999] to words, corresponding to the English pronunciation. 
+
+            Console.Write("Enter a number[0..999]: ");
+            int input = int.Parse(Console.ReadLine());
+            while (input < 0 || input > 999)
+            {
+                Console.Write("Enter a number[0..999]: ");
+                input = int.Parse(Console.ReadLine());
+            }
+
+            string[] fromZerotoNine = {"zero", "one", "two", "three", "four", "five","six","seven", "eight", "nine" };
+            string[] tens = {"ten", "twenty","thirty","forty","fifty" , "sixty","seventy","eighty","ninety"};
+            string[] fromElevenToNineteen = {"eleven", "twelve", "thirteen", "fourteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] hundreds = {"One hundred", "Two hundred", "Three hundred", "Four hundred","Five hundred", "Six hundred", "Seven hundred", "Eight hundred", "Nine hundred" };
+
+            if (input >= 0 && input <= 9)
+            {
+                Console.WriteLine(fromZerotoNine[input].First().ToString().ToUpper() + fromZerotoNine[input].Substring(1));
+            }
+            else if (input >= 11 && input <= 19)
+            {
+                Console.WriteLine(fromElevenToNineteen[(input % 10) - 1].First().ToString().ToUpper() + fromElevenToNineteen[(input % 10) - 1].Substring(1));
+            }
+            else if ((input > 9 && input < 91) && input % 10 == 0)
+            {
+                Console.WriteLine(tens[input / 10 - 1].First().ToString().ToUpper() + tens[input / 10 - 1].Substring(1));
+            }
+            else if (input % 100 == 0)
+            {
+                Console.WriteLine(hundreds[input / 100 - 1]);
+            }
+            else if ((input >= 0 && input <= 99 ) && input % 10 != 0)
+            {
+                int tensCalc = (input - (input % 10)) / 10;
+                Console.WriteLine(tens[tensCalc - 1].First().ToString().ToUpper() + tens[tensCalc - 1].Substring(1) + " " + fromZerotoNine[input % 10]);
+            }
+            else
+            {
+                int hundredsCalc = (input - (input % 100)) / 100;
+                int tensCalc1 = (input % 100);
+                int tensCalc2 = (tensCalc1 - (tensCalc1 % 10)) / 10;
+                int numsCalc = (input % 100) % 10;
+                if (tensCalc1 > 10 && tensCalc1 < 20)
+                {
+                    Console.WriteLine(hundreds[hundredsCalc - 1]+ " and " + fromElevenToNineteen[tensCalc2 - 1]);
+                }
+                else if (numsCalc == 0)
+                {
+                    Console.WriteLine(hundreds[hundredsCalc - 1] + " and " + tens[tensCalc2 - 1]);
+                }
+                else if (tensCalc2 == 0)
+                {
+                    Console.WriteLine(hundreds[hundredsCalc - 1] + " and " + fromZerotoNine[tensCalc1]);
+                }
+                else
+                {
+                    Console.WriteLine(hundreds[hundredsCalc - 1] + " and " + tens[tensCalc2 - 1] + " " + fromZerotoNine[numsCalc]);
+                }
+            }
+        }
+    }
+}
