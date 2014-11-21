66c26cae920fb081ecf9c4637d8551eb7331e302
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2014-11-21 15:43:34 +0200

    Exam Problems added

---
+++ JavaScript Basics/JS-Exam.04.Sep.2014/01.KeepTheChange.js
@@ -0,0 +1,25 @@
+function calculateTip(arr){
+    var bill = parseFloat(arr[0]);
+    var mood = arr[1];
+    var tip = 0;
+
+    if (mood === 'happy') {
+        tip = bill * 0.1;
+    }
+    else if (mood === 'married') {
+        tip = bill * (0.05 / 100);
+    }
+    else if(mood === 'drunk') {
+        tip = bill * 0.15;
+        var toPow = tip;
+        while (toPow >= 10) {
+            toPow = toPow / 10;
+            toPow = parseInt(toPow);
+        }
+        tip = Math.pow(tip, toPow);
+    }
+    else {
+        tip = bill * 0.05;
+    }
+    console.log(tip.toFixed(2));
+}
