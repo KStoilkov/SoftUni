0fa9181970c17ce3c9506536a273a94d4ac126fe
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2015-01-24 02:20:14 +0200

    Problem 4 added

---
+++ OOP/01.DefiningClasses/04.SoftwareUniversityLearningSystem/GraduateStudent.cs
@@ -0,0 +1,12 @@
+﻿namespace _04.SoftwareUniversityLearningSystem
+{
+    class GraduateStudent : Student
+    {
+        public GraduateStudent(string firstName, string lastName, int age, 
+            long studentNumber, double averageGrade)
+            : base(firstName, lastName, age, studentNumber, averageGrade)
+        {
+
+        }
+    }
+}
---
+++ OOP/01.DefiningClasses/04.SoftwareUniversityLearningSystem/OnsiteStudent.cs
@@ -0,0 +1,34 @@
+﻿namespace _04.SoftwareUniversityLearningSystem
+{
+    using System;
+
+    class OnsiteStudent : CurrentStudent
+    {
+        private int numberOfVisits;
+
+        public OnsiteStudent(string firstName, string lastName, int age,
+            long studentNumber, double averageGrade, string currentCourse,
+            int numberOfVisits)
+            : base(firstName, lastName, age, studentNumber, averageGrade, currentCourse)
+        {
+            this.NumberOfVisits = numberOfVisits;
+        }
+
+        public int NumberOfVisits
+        {
+            get 
+            { 
+                return this.numberOfVisits; 
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Number of visits cannot be less than 0.");
+                }
+                this.numberOfVisits = value; 
+            }
+        }
+        
+    }
+}
