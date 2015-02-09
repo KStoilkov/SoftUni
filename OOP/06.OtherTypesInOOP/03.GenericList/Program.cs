8d72f55e5f3dd0944840873c8bfa225344fade2c
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2015-02-09 21:25:36 +0200

    Other types in OOP Homework

---
+++ OOP/06.OtherTypesInOOP/03.GenericList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03.GenericList
+{
+    class Program
+    {
+        static void Main()
+        {
+            GenericList<int> myList = new GenericList<int>();
+            myList.Add(5);
+            myList.Add(1);
+            myList.Add(3);
+            myList.Add(8);
+            Console.WriteLine(myList);
+
+            myList.Remove(2);
+            Console.WriteLine(myList);
+
+            myList.Insert(12, 2);
+            Console.WriteLine(myList);
+
+            myList.FindElementIndex(1);
+            myList.FindElementIndex(17);
+
+            Console.WriteLine(GenericList<int>.Max(5, 12));
+            Console.WriteLine(GenericList<int>.Min(123,3));
+
+            bool test = myList.Contains(8);
+            bool anotherTest = myList.Contains(26);
+            Console.WriteLine(test + " " + anotherTest);
+        }
+    }
+}
