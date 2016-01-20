f35e6414dfa1961ed41de736a38d49eaefce2a73
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2016-01-20 17:24:34 +0200

    XML in .NET homework added

---
+++ Level 3 - Database Applications/05.XML-in-.NET/XML-in-.NET/09.XmlWriter-DirectoryContentsAsXml/DirectoryContentsAsXml.cs
@@ -0,0 +1,46 @@
+﻿namespace _09.XmlWriter_DirectoryContentsAsXml
+{
+    using System;
+    using System.IO;
+    using System.Xml.Linq;
+
+    public class DirectoryContentsAsXml
+    {
+        static void Main()
+        {
+            string path = "C:\\Program Files\\Adobe";
+
+            var element = new XElement("root-dir", new XAttribute("path", path));
+            
+            DirSearch(path, element);
+
+            element.Save("../../../dirAsXml.xml");
+            Console.WriteLine("You can find the .xml file in the solution folder.");
+        }
+
+        public static void DirSearch(string sDir, XElement rootDir)
+        {
+            try
+            {
+                var directories = Directory.GetDirectories(sDir);
+                foreach (string dir in directories)
+                {
+                    var nextDir = new XElement("dir", new XAttribute("name", Path.GetFileName(dir)));
+                    rootDir.Add(nextDir);
+
+                    DirSearch(dir, nextDir);
+
+                    var files = Directory.GetFiles(dir);
+                    foreach (string file in files)
+                    {
+                        nextDir.Add(new XElement("file", new XAttribute("name", Path.GetFileName(file))));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
