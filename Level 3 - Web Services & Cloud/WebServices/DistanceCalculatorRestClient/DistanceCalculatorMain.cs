0a24b18ca7df6e7161a0d260fa40d82ef28cd074
Kiril Stoilkov <K.I.Stoilkov@gmail.com>
2016-01-27 12:23:52 +0200

    Web Services homework added

---
+++ Level 3 - Web Services & Cloud/WebServices/DistanceCalculatorRestClient/DistanceCalculatorMain.cs
@@ -0,0 +1,24 @@
+﻿namespace DistanceCalculatorRestClient
+{
+    using System;
+    using System.IO;
+    using System.Net;
+
+    public class DistanceCalculatorMain
+    {
+        static void Main()
+        {
+            string url = "http://localhost:59860/api/points?x1=10&y1=10&x2=15&y2=15";
+
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = reader.ReadToEnd();
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
