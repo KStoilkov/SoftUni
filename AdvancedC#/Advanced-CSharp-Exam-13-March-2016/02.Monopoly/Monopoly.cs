3614438fbb2cd4b7404e619460003c96dc2e6845
Kiril Stoilkov <Kiril Stoilkov>
2016-06-19 17:49:08 +0300

    Advanced C# Exam tasks 2 and 3 added.

---
+++ AdvancedC#/Advanced-CSharp-Exam-13-March-2016/02.Monopoly/Monopoly.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Monopoly
+{
+    class Monopoly
+    {
+        public static char hotel = 'H';
+        public static char free = 'F';
+        public static char jail = 'J';
+        public static char shop = 'S';
+
+        public static int turn = 0;
+        public static int money = 50;
+        public static int hotelsCount = 0;
+     
+        public static bool isHotelHited = false;
+
+        public static char[,] monopolyField = null;
+        public static bool flag = false;
+
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            
+            monopolyField = new char[dimensions[0], dimensions[1]];
+        
+
+            for (int i = 0; i < dimensions[0]; i++)
+            {
+                char[] field = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < field.Length; j++)
+                {
+                    monopolyField[i,j] = field[j];
+                }
+            }
+
+            for (int i = 0; i < monopolyField.GetLength(0); i++)
+            {
+                if (flag)
+                {
+                    for (int j = monopolyField.GetLength(1) - 1; j >= 0; j--)
+                    {
+                        checkField(i, j);
+                        turn++;
+                    }
+                    flag = !flag;
+                }
+                else
+                {
+                    for (int j = 0; j < monopolyField.GetLength(1); j++)
+                    {
+                        checkField(i, j);
+                        turn++;
+                    }
+                    flag = !flag;
+                }
+            }
+
+            Console.WriteLine("Turns "+turn+"");
+            Console.WriteLine("Money "+money+"");
+        }
+
+        public static void checkField(int posX, int posY)
+        {
+            char currentField = monopolyField[posX, posY];
+            if (currentField == hotel)
+            {
+                hotelsCount++;
+                Console.WriteLine("Bought a hotel for " + money + ". Total hotels: " + hotelsCount + ".");
+                isHotelHited = true;
+                money = 0;
+            }
+
+            if (currentField == jail)
+            {
+                Console.WriteLine("Gone to jail at turn " + turn + ".");
+                turn += 2;
+                money += (2 * 10) * hotelsCount;
+            }
+
+            if (currentField == shop)
+            {
+                int spended = (posX + 1) * (posY + 1);
+                Console.WriteLine("Spent " + spended + " money at the shop.");
+                money -= spended;
+            }
+
+            if (isHotelHited)
+            {
+                money += 10 * hotelsCount;
+            }
+        }
+    }
+}
---
+++ AdvancedC#/Advanced-CSharp-Exam-13-March-2016/03.BasicMarkUpLanguage/BasicMarkUpLanguage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.BasicMarkUpLanguage
+{
+    class BasicMarkUpLanguage
+    {
+        public static string cont = "content=\"";
+        public static string val = "value=\"";
+        public static int counter = 1;
+
+        static void Main()
+        {
+            List<string> output = new List<string>();
+            string[] newcom = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Trim())
+                .ToArray();
+
+            string command = String.Join(" ", newcom);
+            
+
+            while (command != "<stop/>")
+            {
+                string content = GetContent(command);
+                if (command.Contains("reverse"))
+                {
+                    var a = content.Reverse();
+                    output.Add(counter + ". " + String.Join("", a));
+                }
+                if (command.Contains("inverse"))
+                {
+                    string outp = string.Empty;
+                    foreach (char c in content)
+                    {
+                        if (Char.IsUpper(c))
+                        {
+                            outp += Char.ToLower(c);
+                        }
+                        else
+                        {
+                            outp += Char.ToUpper(c);
+                        }
+                    }
+                    output.Add(counter + ". " + outp);
+                }
+                if (command.Contains("repeat"))
+                {
+                    int value = GetValue(command);
+                    while (value > 0)
+                    {
+                        output.Add(counter + ". " + content);
+                        counter++;
+                        value--;
+                    }
+                }
+                counter++;
+                command = Console.ReadLine();
+            }
+
+            foreach (string o in output)
+            {
+                Console.WriteLine(o);
+            }
+        }
+
+        private static int GetValue(string command)
+        {
+            int index = command.IndexOf(val) + val.Length;
+            string rest = command.Substring(index, command.Length - index);
+            string content = rest.Substring(0, rest.IndexOf("\""));
+            return int.Parse(content);
+        }
+
+        private static string GetContent(string command)
+        {
+            int index = command.IndexOf(cont) + cont.Length;
+            string rest = command.Substring(index, command.Length - index - 1);
+            string content = rest.Substring(0, rest.IndexOf("\""));
+            return content;
+        }
+    }
+}
